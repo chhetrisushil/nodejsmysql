34dcac96ddedfcfbba3d9e288b03fb29f8f1e751
Sushil Chhetri <chhetrisushil@gmail.com>
2013-01-24 16:22:54 +0530

    API's

--- routes.js
+++ routes.js
@@ -12,7 +12,12 @@ var mysql = require('mysql'),
                 return;
             }
             
-            res.end(JSON.stringify(rows));
+            res.end(JSON.stringify({
+                success: true,
+                totalRows: rows.length,
+                query: '/tasks',
+                resultSet: rows
+            }));
             /* for (var i = 0, len = rows.length, row; i < len; i++) {
                 row = rows[i];
                 for (var j in row) {
@@ -27,25 +32,140 @@ var mysql = require('mysql'),
     },
     
     addTask: function (req, res) {
-        res.end();
+        console.log(req.body);
+        res.end(JSON.stringify(req.body));
     },
     
     getTaskById: function (req, res) {
-        var id = req.params.id;
-        
-        res.end(id);
+        var id = req.params.id,
+            result;
+        if (!id) {
+            connection.query('SELECT * from taskitems ORDER BY taskid', function (err, rows, fields) {
+                if (err) {
+                    console.log('error: ', err);
+                    return;
+                }
+                
+                res.end(JSON.stringify({
+                    success: true,
+                    totalRows: rows.length,
+                    query: '/id/',
+                    resultSet: rows
+                }));
+            });
+        } else {
+            connection.query('SELECT * from taskitems WHERE taskid='+id, function (err, rows, fields) {
+                var result;
+                if (err) {
+                    console.log('error: ', err);
+                    return;
+                }
+                if (rows.length) {
+                    result = JSON.stringify({
+                        success: true,
+                        totalRows: rows.length,
+                        query: '/tag/'+id,
+                        resultSet: rows
+                    });
+                } else {
+                    result = JSON.stringify({
+                        success: false,
+                        totalRows: 0,
+                        query: '/tag/'+id,
+                        resultSet: []
+                    });
+                }
+                res.end(result);
+            });
+        }
     },
     
     getTaskByPriority: function (req, res) {
         var priority = req.params.priority;
         
-        res.end(priority);
+        if (!priority) {
+            connection.query('SELECT * from taskitems ORDER BY priority', function (err, rows, fields) {
+                if (err) {
+                    console.log('error: ', err);
+                    return;
+                }
+                console.log(fields);
+                res.end(JSON.stringify({
+                    success: true,
+                    totalRows: rows.length,
+                    query: '/priority/',
+                    resultSet: rows
+                }));
+            });
+        } else {
+            connection.query('SELECT * from taskitems WHERE priority="'+priority+'"', function (err, rows, fields) {
+                var result;
+                if (err) {
+                    console.log('error: ', err);
+                    return;
+                }
+                if (rows.length) {
+                    result = JSON.stringify({
+                        success: true,
+                        totalRows: rows.length,
+                        query: '/priority/'+priority,
+                        resultSet: rows
+                    });
+                } else {
+                    result = JSON.stringify({
+                        success: false,
+                        totalRows: 0,
+                        query: '/priority/'+priority,
+                        resultSet: []
+                    });
+                }
+                res.end(result);
+            });
+        }
     },
     
     getTaskByTag: function (req, res) {
         var tag = req.params.tag;
         
-        res.end(tag);
+        if (!tag) {
+            connection.query('SELECT * from taskitems ORDER BY tag', function (err, rows, fields) {
+                if (err) {
+                    console.log('error: ', err);
+                    return;
+                }
+                console.log(fields);
+                res.end(JSON.stringify({
+                    success: true,
+                    totalRows: rows.length,
+                    query: '/tag/',
+                    resultSet: rows
+                }));
+            });
+        } else {
+            connection.query('SELECT * from taskitems WHERE tag="'+tag+'"', function (err, rows, fields) {
+                var result;
+                if (err) {
+                    console.log('error: ', err);
+                    return;
+                }
+                if (rows.length) {
+                    result = JSON.stringify({
+                        success: true,
+                        totalRows: rows.length,
+                        query: '/tag/'+tag,
+                        resultSet: rows
+                    });
+                } else {
+                    result = JSON.stringify({
+                        success: false,
+                        totalRows: 0,
+                        query: '/tag/'+tag,
+                        resultSet: []
+                    });
+                }
+                res.end(result);
+            });
+        }
     },
     
     updateTask: function (req, res) {
--- server.js
+++ server.js
@@ -7,29 +7,31 @@ var express = require('express'),
         user: 'root'
     }),
     routes = require('./routes.js');
+// use express.bodyParser to parse request body for post request
+app.use(express.bodyParser());
 
 // get the task list
 app.get('/tasks', routes.getTasksList);
 
 //get task by id
-app.get('/tasks/id/:id', routes.getTaskById);
-app.get('/tasks/id', routes.getTaskById); // if id is not specified return task list sorted by id (descending)
+app.get('/task/id/:id', routes.getTaskById);
+app.get('/task/id', routes.getTaskById); // if id is not specified return task list sorted by id (descending)
 
 //get task by priority
-app.get('/tasks/priority/:priority', routes.getTaskByPriority);
-app.get('/tasks/priority', routes.getTaskByPriority); // if priority is not specified return task list sorted by priority (descending)
+app.get('/task/priority/:priority', routes.getTaskByPriority);
+app.get('/task/priority', routes.getTaskByPriority); // if priority is not specified return task list sorted by priority (descending)
 
 //get task by tag
-app.get('/tasks/tag/:tag', routes.getTaskByTag);
-app.get('/tasks/tag', routes.getTaskByTag); // if tag is not specified return task list sorted by tag (alphabetic order: ascending)
+app.get('/task/tag/:tag', routes.getTaskByTag);
+app.get('/task/tag', routes.getTaskByTag); // if tag is not specified return task list sorted by tag (alphabetic order: ascending)
 
 //create new task
-app.post('/tasks', routes.addTask);
+app.post('/task/create', routes.addTask);
 
 //update a task
-app.put('/tasks/:id', routes.updateTask);
+app.put('/task/update/:id', routes.updateTask);
 
 //delete a task
-app.delete('/tasks/:id', routes.deleteTask);
+app.delete('/task/delete/:id', routes.deleteTask);
 
 app.listen('3000');
